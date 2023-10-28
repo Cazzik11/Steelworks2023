fb9c2586e939b6936dca04e47e242bf408bdf0f7
Maciej Maniecki <manieckimaciej@gmail.com>
2023-10-29 01:11:02 +0200

    LeanTween plugin, dialogue system fixes

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -14,51 +14,51 @@ public class DialogueManager : MonoBehaviour
     Actor[] currentActors;
     int activeMessage = 0;
     public static bool isActive = false;
-
-    public void OpenDialogue(Message[] messages, Actor[] actors) 
-    {
+    public void OpenDialogue(Message[] messages, Actor[] actors) {
         currentMessages = messages;
         currentActors = actors;
         activeMessage = 0;
         isActive = true;
         Debug.Log("Started conversation! Loaded messages: " + messages.Length);
         DisplayMessage();
+        backgroundBox.LeanScale(Vector3.one, 0.5f);
     }
-
-    void DisplayMessage() 
-    {
+    void DisplayMessage() {
         Message messageToDisplay = currentMessages[activeMessage];
         messageText.text = messageToDisplay.message;
 
         Actor actorToDisplay = currentActors[messageToDisplay.actorId];
         actorName.text = actorToDisplay.name;
         actorImage.sprite = actorToDisplay.sprite;
+
+        AnimateTextColor();
     }
-    
-    public void NextMessage()
-    {
+    public void NextMessage() {
         activeMessage++;
         if (activeMessage < currentMessages.Length) {
             DisplayMessage();
-        } 
-        else
-        {
+        } else {
             Debug.Log("Conversation ended!");
+            backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
             isActive = false;
         }
+    }
+
+    void AnimateTextColor() {
+        LeanTween.textAlpha(messageText.rectTransform, 0, 0);
+        LeanTween.textAlpha(messageText.rectTransform, 1, 0.5f);
     }
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        backgroundBox.transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isActive == true)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == true) {
             NextMessage();
         }
     }
